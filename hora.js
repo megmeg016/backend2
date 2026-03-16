0ccefc8689c064c8018f3cbda6e5cd4b48cf7742
megmegmegmegemgemgemgemegm <maria.gomes.nascimento@escola.pr.gov.br>
2026-03-16 13:54:44 +0000

    versao1

--- hora.js
+++ hora.js
@@ -1,1 +1,17 @@
-//Valoresniniciais do dia
+// Valores iniciais do dia
+let valorPorHora = 50;
+let horas Trabalhadas = 8; // Expediente padrão
+
+console.log("--- Resumo do Expediente ---");
+console.log('Horas iniciais: ${horas Trabalhadash`);
+
+horas Trabalhadas++;
+console.log('Após hora extra (horas Trabalhadas++): ${horas Trabalhadas}h');
+
+//No dia seguinte ele precisou sair uma hora mais cedo para ir ao médico
+horas Trabalhadas--
+console.log('Após saída antecipada (horas Trabalhadas--): ${horas Trabalhadas}h`);
+
+    //Cálculo final
+let taxaSistema = 15;
+let taxaSistema = (horasTrabalhadas = valorPorHora) - taxaSistema;
