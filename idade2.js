11062317e1e2743588bf0aa78768cac95933ab41
megmegmegmegemgemgemgemegm <maria.gomes.nascimento@escola.pr.gov.br>
2026-03-25 12:43:26 +0000

    versao1

---
+++ idade2.js
@@ -0,0 +1,10 @@
+const nome =  "maria"
+const age = 10;
+
+if(age >= 18) {
+    console.log(nome + " é maior de idade.")
+} else if (age === 17) {
+    console.log( nome + " tem 17 anos = quase lá!")
+} else {
+      console.log( nome + " é menor de idade.")
+}
