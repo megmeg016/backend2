c9295c781dd032401b5e7f52d02991ec479e5e58
megmegmegmegemgemgemgemegm <maria.gomes.nascimento@escola.pr.gov.br>
2026-04-14 11:41:51 +0000

    versao1

---
+++ array.js
@@ -0,0 +1,14 @@
+const frutas = ["Maça", "Banana", "Uva", "Manga"];
+
+console.log(frutas[1]);
+console.log(frutas[3]);
+console.log(frutas.length);
+frutas[1] = "Morango";
+frutas.push("Pêssego");
+frutas.unshift("pera");
+console.log(frutas);
+console.log(frutas.length);
+
+frutas[3] = "Abacate";
+frutas.push("Melão");
+console.log(frutas);
