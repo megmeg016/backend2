eef1964005614c93adc383362a1c1cd1f46ede2c
megmegmegmegemgemgemgemegm <maria.gomes.nascimento@escola.pr.gov.br>
2026-04-27 11:53:24 +0000

    v1

---
+++ objeto.js
@@ -0,0 +1,19 @@
+const bancoautomotivo = {
+    material: "Couro Natural",
+    cor: "Caramelo",
+    tamanho: "Padrão",
+    concluido: false,
+}
+
+//console.log("Material escolhido: ", bancoautomotivo.material);
+
+   //bancoautomotivo.concluido = true;
+   //     console.log(bancoautomotivo)
+
+   const papelaria = {
+    canetas: "Bic Multicolors",
+    mochilas: "Capivaras fofinhas",
+    folhas: "almaço e sulfite",
+    borrachas: "de desenhos",
+    garrafas: "Coloridas",
+   }
