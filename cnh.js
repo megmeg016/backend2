858448411a1b9dae341fd6def9f278dc408eb36a
megmegmegmegemgemgemgemegm <maria.gomes.nascimento@escola.pr.gov.br>
2026-04-28 11:25:41 +0000

    versao1

--- cnh.js
+++ cnh.js
@@ -1,3 +1,10 @@
 const age = 17;
 const maiorIdade = age >=18;
 const possuiCNH = false;
+
+// Valores iniciais do dia
+let valorPorHora = 50;
+let horasTrabalhadas = 8; // Expediente padrão
+
+console.log("--- Resumo do Expediente ---");
+console.log(`Horas iniciais: ${horasTrabalhadas}h`);
