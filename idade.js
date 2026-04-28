8d12d70bb951a7fd8b9f8f9a2388f53dc4001a5f
megmegmegmegemgemgemgemegm <maria.gomes.nascimento@escola.pr.gov.br>
2026-04-28 11:41:01 +0000

    versao1

---
+++ idade.js
@@ -0,0 +1,6 @@
+let idade = 18;
+let dataNascimento = 2008;
+
+//console.log("Minha idade é: " + idade);
+//console.log("Meu ano de nascimento é: " + dataNascimento);
+console.log(`Minha idade é: ${idade} e meu ano de nascimento é: ${dataNascimento}`);
